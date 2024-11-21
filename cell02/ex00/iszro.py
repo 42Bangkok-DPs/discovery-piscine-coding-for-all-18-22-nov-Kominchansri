ea869a48c6a5508d2ac2ab60505243863dd3d58d
Kominchansri <minchansri560@gmail.com>
2024-11-21 04:07:54 +0000

    iszero.py

--- cell02/ex00/iszro.py
+++ cell02/ex00/iszro.py
@@ -1,5 +1,5 @@
 number = fioat(input("enter a number: "))
 if number == 0:
-    print("This number is equal to zero")
-    else:
-        print("This number is different from zero")
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
